b2f3c539458a2069e1330e13b8311369ca71f4f7
xiebin2015 <xiebin163126@163.com>
2015-12-01 16:55:46 +0800

    添加 返回接口url

--- pay-autotest/pay-autotest-threadpool/src/main/java/com/gigold/pay/autotest/threadpool/CheckThread.java
+++ pay-autotest/pay-autotest-threadpool/src/main/java/com/gigold/pay/autotest/threadpool/CheckThread.java
@@ -47,11 +47,12 @@ public class CheckThread implements Runnable {
     		int ifId=Integer.parseInt(String.valueOf(ifmap.get("ID")));
     		List<Map<String,Object>> mockList=ifSysMockService.getMockInfoByIfId(ifId);
     		for(Map<String,Object> mockmap:mockList){
-    			//设置 接口名、接口描述、接口所属系统名、产品名、服务器地址
+    			//设置 接口名、接口描述、接口所属系统名、产品名、接口请求地址，服务器地址
     			mockmap.put("IF_NAME", ifmap.get("IF_NAME"));
     			mockmap.put("IF_DESC", ifmap.get("IF_DESC"));
     			mockmap.put("SYS_NAME", ifmap.get("SYS_NAME"));
     			mockmap.put("PRO_NAME", ifmap.get("PRO_NAME"));
+    			mockmap.put("IF_URL", ifmap.get("IF_URL"));
     			mockmap.put("ADDRESS_URL", ifmap.get("ADDRESS_URL"));
     			//调用 httpclient代码访问接口
     			
