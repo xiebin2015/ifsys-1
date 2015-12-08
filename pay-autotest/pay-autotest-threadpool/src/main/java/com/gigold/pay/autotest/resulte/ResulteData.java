9517cf95568dd7e326a5e77bb648697a706ee4a2
chenkuan <393667111@qq.com>
2015-12-08 15:57:46 +0800

    新增测试结果获取方法

---
+++ pay-autotest/pay-autotest-threadpool/src/main/java/com/gigold/pay/autotest/resulte/ResulteData.java
@@ -0,0 +1,12 @@
+package com.gigold.pay.autotest.resulte;
+
+import net.sf.json.JSONObject;
+
+/**
+ * Created by chenkuan on 15/12/8.
+ */
+public interface ResulteData {
+    public int count=0;
+    public void addResulte();
+    public JSONObject getResulte();
+}
