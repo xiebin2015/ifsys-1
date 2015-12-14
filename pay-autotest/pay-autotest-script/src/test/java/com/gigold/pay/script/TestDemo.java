f42b6c62113d35a6f38562b98ce62612df6e2588
xiebin2015 <xiebin163126@163.com>
2015-12-14 10:36:58 +0800

    注释掉 邮件抄送人的设置

--- pay-autotest/pay-autotest-script/src/test/java/com/gigold/pay/script/TestDemo.java
+++ pay-autotest/pay-autotest-script/src/test/java/com/gigold/pay/script/TestDemo.java
@@ -7,18 +7,12 @@
  */
 package com.gigold.pay.script;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Iterator;
 
-
-import com.alibaba.dubbo.common.json.JSON;
-import com.gigold.pay.autotest.bo.InterFaceInfo;
-import com.gigold.pay.framework.bootstrap.SystemPropertyConfigure;
-import net.sf.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -27,10 +21,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.gigold.pay.autotest.bo.IfSysMock;
 import com.gigold.pay.autotest.email.MailSenderService;
-import com.gigold.pay.autotest.resulte.ResulteData;
 import com.gigold.pay.autotest.service.IfSysMockService;
 import com.gigold.pay.autotest.threadpool.IfsysCheckThreadPool;
 import com.gigold.pay.framework.base.SpringContextHolder;
+import com.gigold.pay.framework.bootstrap.SystemPropertyConfigure;
 
 /**
  * Title: Test<br/>
@@ -98,10 +92,10 @@ public class TestDemo {
 				}
 			}
 		}
-
-		// 2.设置抄送人地址
-		String[] copyList = SystemPropertyConfigure.getProperty("mail.default.observer").split(",");
-		mailSenderService.setCc(copyList);
+//
+//		// 2.设置抄送人地址
+//		String[] copyList = SystemPropertyConfigure.getProperty("mail.default.observer").split(",");
+//		mailSenderService.setCc(copyList);
 
 		// 3.分发收件人
 		Iterator entries = mailBuffers.entrySet().iterator();
