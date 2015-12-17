7cdb637ef2f3a24b019ca0d542ec89f0b9d2c8d0
xiebin2015 <xiebin163126@163.com>
2015-12-17 15:53:04 +0800

    添加 单元测试

---
+++ ifsys/src/test/java/com/gigold/pay/ifsys/service/InterFaceFieldServiceTest.java
@@ -0,0 +1,69 @@
+/**
+ * Title: InterFaceFieldServiceTest.java<br/>
+ * Description: <br/>
+ * Copyright: Copyright (c) 2015<br/>
+ * Company: gigold<br/>
+ *
+ */
+package com.gigold.pay.ifsys.service;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.gigold.pay.ifsys.bo.InterFaceField;
+import com.gigold.pay.ifsys.bo.InterFaceInfo;
+import com.gigold.pay.ifsys.dao.InterFaceDao;
+import com.gigold.pay.ifsys.dao.InterFaceFieldDao;
+
+/**
+ * Title: InterFaceFieldServiceTest<br/>
+ * Description: <br/>
+ * Company: gigold<br/>
+ * 
+ * @author xiebin
+ * @date 2015年12月17日下午3:11:22
+ *
+ */
+@RunWith(PowerMockRunner.class)
+public class InterFaceFieldServiceTest {
+	/** ====================== mock对象定义 ========================== **/
+	@Mock
+	InterFaceFieldDao interFaceFieldDao;
+	/** ====================== 测试对象定义 ========================== **/
+	private InterFaceFieldService interFaceFieldService = new InterFaceFieldService();
+
+	/**
+	 * mock对象初始化
+	 *
+	 * @throws Exception
+	 *             the exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		// 测试对象中被mock对象赋值
+		interFaceFieldService.setInterFaceFieldDao(interFaceFieldDao);
+	}
+
+	@Test
+	public void getFirstReqFieldByIfId() {
+		InterFaceField interFaceField = new InterFaceField();
+		when(interFaceFieldDao.getFirstReqFieldByIfId(any(InterFaceField.class))).thenReturn(null)
+				.thenReturn(new ArrayList());
+		List<InterFaceField> list = interFaceFieldService.getFirstReqFieldByIfId(interFaceField);
+		// 获取失败 包括抛异常的情况
+		Assert.assertNull(list);
+		list = interFaceFieldService.getFieldByIfId(interFaceField);
+		// 成功
+		Assert.assertNotNull(list);
+	}
+}
