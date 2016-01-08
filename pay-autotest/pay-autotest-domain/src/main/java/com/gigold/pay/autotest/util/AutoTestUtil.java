14903eeba9eea002df48ffb701b18581393943a0
xiebin2015 <xiebin163126@163.com>
2016-01-08 10:09:51 +0800

    添加 根据规则自动生成测试数据的方法

--- pay-autotest/pay-autotest-domain/src/main/java/com/gigold/pay/autotest/util/AutoTestUtil.java
+++ pay-autotest/pay-autotest-domain/src/main/java/com/gigold/pay/autotest/util/AutoTestUtil.java
@@ -7,6 +7,11 @@
  */
 package com.gigold.pay.autotest.util;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Random;
+
+import org.apache.commons.lang.RandomStringUtils;
+
 import com.gigold.pay.framework.util.common.StringUtil;
 
 import net.sf.json.JSONObject;
@@ -20,6 +25,45 @@ import net.sf.json.JSONObject;
  *
  */
 public class AutoTestUtil {
+	
+	
+		private static final String cellStart[]=new String[]{
+				//移动
+				"134",
+				"135",
+				"136",
+				"137",
+				"138",
+				"139",
+				"147",
+				"150",
+				"151",
+				"152",
+				"157",
+				"158",
+				"159",
+				"182",
+				"187",
+				"188",
+				//联通
+				"130",
+				"131",
+				"132",
+				"155",
+				"156",
+				"185",
+				"186",
+				"186",
+				"186",
+                //电信
+				"133",
+				"153",
+				"180",
+				"189"
+		};
+	
+	
+	
     /**
      * 
      * Title: proTestData<br/>
@@ -32,12 +76,98 @@ public class AutoTestUtil {
      * @return
      */
 	public static String proTestDataByReg(String reg,String length){
-		//规则1:生成随即字符串
-		if("1".equals(reg)){
-			
+		String regStr="";
+		//默认生成六位
+		int len=6;
+		try{
+			len=Integer.parseInt(length);
+		}catch(Exception e){
+			len=6;
 		}
-		return null;
+		
+		switch (reg) {
+		case "1" :
+			//纯数字字符串
+			regStr=RandomStringUtils.randomNumeric(len);
+			break;
+		case "2" :
+			//生成符合邮箱规则的字符串 不保证其真实性
+			//10为随随机字符@3位随机字符.com
+			regStr=	RandomStringUtils.randomAlphanumeric(10)+"@"+RandomStringUtils.randomAlphanumeric(3)+".com";
+			break;
+		case "3" :
+			//生成手机号
+			int index=ramdoMuberRanger(1,27);
+			regStr=cellStart[index]+RandomStringUtils.randomNumeric(8);
+			break;
+		case "4" :
+			//生成随机中文字符串
+			regStr=getRandomJianHan(len);
+			break;
+		default:
+			//生成随机字符串 数字字母组合
+			regStr=RandomStringUtils.randomAlphabetic(len);
+			regStr=regStr.toLowerCase();
+		}
+		
+			
+	
+		return regStr;
 	}
+	
+	
+	/**
+	 * 
+	 * Title: getRandomJianHan<br/>
+	 * Description: 获取指定长度随机简体中文<br/>
+	 * @author xiebin
+	 * @date 2016年1月8日上午10:06:09
+	 *
+	 * @param len
+	 * @return
+	 */
+    public static String getRandomJianHan(int len)
+    {
+        String ret="";
+          for(int i=0;i<len;i++){
+              String str = null;
+              int hightPos, lowPos; // 定义高低位
+              Random random = new Random();
+              hightPos = (176 + Math.abs(random.nextInt(39))); //获取高位值
+              lowPos = (161 + Math.abs(random.nextInt(93))); //获取低位值
+              byte[] b = new byte[2];
+              b[0] = (new Integer(hightPos).byteValue());
+              b[1] = (new Integer(lowPos).byteValue());
+              try
+              {
+                  str = new String(b, "GBk"); //转成中文
+              }
+              catch (UnsupportedEncodingException ex)
+              {
+                  ex.printStackTrace();
+              }
+               ret+=str;
+          }
+      return ret;
+    }
+    
+	/**
+	 * 
+	 * Title: ramdoMuberRanger<br/>
+	 * Description: 生成某个范围内的随机数字 [min,max]<br/>
+	 * @author xiebin
+	 * @date 2016年1月8日上午9:56:57
+	 *
+	 * @param min
+	 * @param max
+	 * @return
+	 */
+	public static int ramdoMuberRanger(int min,int max){
+        Random random = new Random();
+        int s = random.nextInt(max)%(max-min+1) + min;
+        return s;
+	}
+	
 	/**
 	 * 
 	 * Title: proTestDataByRspBody<br/>
